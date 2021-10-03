f88789087f52a445c736b5da3177d3ed42e67f1a
snehadaripa <71598352+snehadaripa@users.noreply.github.com>
2021-10-03 23:58:07 +0530

    Create DivisionOfIntegers.py

---
+++ DivisionOfIntegers.py
@@ -0,0 +1,9 @@
+//Division of Integers
+number1=int(input("Enter the first number: "))
+number2=int(input("Enter the second number: "))
+result=number1/number2
+//Integer division
+print(int(result))
+
+//Float Division
+print(result)
