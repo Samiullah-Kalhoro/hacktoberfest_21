056aa6c1202dffe567777f84061e1055fb99b985
Harshit Poddar <89685284+Harshit-Poddar90@users.noreply.github.com>
2021-10-06 09:29:09 +0530

    Create Write a function.py

---
+++ Write a function/Write a function.py
@@ -0,0 +1,10 @@
+def is_leap(year):
+    if year % 400 == 0:
+        return True
+    if year % 100 == 0:
+        return False
+    if year % 4 == 0:
+        return True
+    else:
+        return False
+
